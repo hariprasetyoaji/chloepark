323f76d9ff4278f42e70ac58ad1f8aa6597a4d36
Nico Taing <ntaing@opentable.com>
2015-09-16 19:52:27 -0700

    [theme/park] .container -> .homepage

--- themes/park/source/javascript/main.js
+++ themes/park/source/javascript/main.js
@@ -2,11 +2,11 @@
 (function () {
   'use strict';
 
-  var $buffer = document.getElementById('container__buffer');
+  var $buffer = document.getElementById('homepage__buffer');
 
   function imageReady () {
     var classNames = $buffer.className;
-    $buffer.setAttribute('class', classNames + ' container__buffer--ready');
+    $buffer.setAttribute('class', classNames + ' homepage__buffer--ready');
   }
 
   $buffer.onload = imageReady;
