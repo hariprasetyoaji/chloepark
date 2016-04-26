e057646fd86d55cfc4ce421820ee6e59aa6ba86b
Nico Taing <ntaing@opentable.com>
2016-04-25 23:04:23 -0700

    [theme/park] add transitions

--- themes/park/source/javascript/main.js
+++ themes/park/source/javascript/main.js
@@ -2,12 +2,40 @@
 (function () {
   'use strict';
 
+  // Transition to display background
   var $buffer = document.getElementById('homepage__buffer');
 
   function imageReady () {
-    var classNames = $buffer.className;
-    $buffer.setAttribute('class', classNames + ' homepage__buffer--ready');
+    $buffer.classList.add('homepage__buffer--ready');
   }
 
   $buffer.onload = imageReady;
+
+  // Transition for social links
+  var $socialLinksList = document.getElementById('homepage__social-links');
+  var $socialLinks = document.getElementsByClassName('homepage__social-link');
+  var linkTransparentClass= 'homepage__social-link--transparent';
+
+  function mouseEnterHandler (evt) {
+    for (var i = 0; i < $socialLinks.length; i++) {
+      if (evt.target === $socialLinks[i]) { continue; }
+      var classNames = $socialLinks[i].className;
+      $socialLinks[i].classList.add(linkTransparentClass);
+    }
+  }
+
+  function mouseLeaveHandler (evt) {
+    for (var i = 0; i < $socialLinks.length; i++) {
+      $socialLinks[i].classList.remove(linkTransparentClass);
+    }
+  }
+
+  function addMouseEventListener ($links) {
+    for (var i = 0; i < $links.length; i++) {
+      $links[i].addEventListener('mouseenter', mouseEnterHandler);
+      $links[i].addEventListener('mouseleave', mouseLeaveHandler);
+    }
+  }
+
+  addMouseEventListener($socialLinks);
 })();
